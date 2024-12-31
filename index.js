a46425af731853f65004528908529faa4751df83
FollowNaim <naimxf@gmail.com>
2024-12-31 10:54:59 +0600

    feat(api): created pagination api and total documents count
    Signed-off-by: FollowNaim <naimxf@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("savorly server is running");
@@ -23,6 +26,22 @@ const run = async () => {
   try {
     await client.connect();
     console.log("mongodb connected successfully!");
+    const dishesCollection = client.db("savorly").collection("dishes");
+    app.get("/dishes", async (req, res) => {
+      const count = req.query.count;
+      const size = req.query.size;
+      const page = req.query.page;
+      if (count) {
+        const result = await dishesCollection.estimatedDocumentCount();
+        return res.json({ count: result });
+      }
+      const result = await dishesCollection
+        .find()
+        .limit(parseInt(size))
+        .skip(page * size)
+        .toArray();
+      res.send(result);
+    });
   } catch (err) {
     console.log(err);
   }
